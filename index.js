13db11cd0b90bc52bc6af9c93b10fe44737fa284
Mario Marinero <mario.marinero@scalefast.com>
2021-02-02 22:41:27 +0100

    Fix import extensions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import conf from "./src/conf/index";
-import Bot from "./src/bot";
-import Lichess from "./src/lichess/lichess";
+import conf from "./src/conf/index.js";
+import Bot from "./src/bot.js";
+import Lichess from "./src/lichess/lichess.js";
 
 const lichess = new Lichess(conf.lichessApiToken);
 const bot = new Bot(conf.telegramBotToken, lichess);
