01cd72e359674bd216c04484242703571b2d9360
Ana Arriaga Coll <anaarriagacoll@gmail.com>
2021-02-14 00:29:48 +0100

    Chessito - connect to mongoDB database with mongoose

--- connect-mongoose.js
+++ connect-mongoose.js
@@ -1,20 +1,23 @@
-import mongodb from 'mongodb';
+import mongoose from 'mongoose';
 import conf from "./src/conf/index.js";
 
-const { MongoClient } = mongodb; 
 const { dbUserName, dbPassword, dbName } = conf.mongodb;                                                                                                                  
 const url = `mongodb+srv://${dbUserName}:${dbPassword}@karma-order.g4csj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-const client = new MongoClient(url);
 async function run() {
     try {
-        await client.connect();
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
         console.log("Connected correctly to server");
     } catch (err) {
         console.log(err.stack);
     }
     finally {
-        await client.close();
+        await mongoose.connection.close();
     }
 }
 run().catch(console.dir);
